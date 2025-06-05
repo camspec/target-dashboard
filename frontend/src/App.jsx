b41d82e87e6d6a5e9aa3511bbbe24e66db401a4c
Cameron Lee <cameronkmlee10@gmail.com>
2025-06-04 21:58:09 -0400

    Add submit button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,32 @@ import "./App.css";
 function App() {
   const [apiKey, setApiKey] = useState("");
 
+  const handleChange = (e) => {
+    setApiKey(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`API Key entered: ${apiKey}`);
+  };
+
   return (
     <>
       <h1>IntelGrid</h1>
-      <label htmlFor="api-key-input">Enter your Limited Access API Key: </label>
-      <input
-        id="api-key-input"
-        type="text"
-        value={apiKey}
-        onChange={(e) => setApiKey(e.target.value)}
-        placeholder="Your API Key"
-      />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="api-key-input">
+          Enter your Limited Access API Key:{" "}
+        </label>
+        <input
+          id="api-key-input"
+          type="text"
+          value={apiKey}
+          onChange={handleChange}
+          placeholder="Your API Key"
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
